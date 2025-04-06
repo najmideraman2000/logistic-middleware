263c4ec10d32bc03874d171bbc0662191bd2c393
najmi <najmideraman2000@gmail.com>
2025-04-06 13:27:23 +0800

    init

---
+++ logistic-middleware/src/main/java/com/najmi/tmrnd/logistic_middleware/LogisticMiddlewareApplication.java
@@ -0,0 +1,13 @@
+package com.najmi.tmrnd.logistic_middleware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticMiddlewareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticMiddlewareApplication.class, args);
+	}
+
+}
